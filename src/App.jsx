b4b71f09e05ab9922375f8bdbae8f4cebfd7f5bc
Shashi Kiran Perka <shashikira4124@gmail.com>
2025-07-21 05:47:36 +0530

    fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import styles from "/src/App.module.css";
+import "./App.css"; 
+
 const API_URL = "https://api.weatherapi.com/v1/current.json";
 const API_KEY = "baee6b3cfa614321b1f230524252007";
 
@@ -9,10 +10,8 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const [showAlert, setShowAlert] = useState(false);
-  const [alertMsg, setAlertMsg] = useState("");
 
   function showCustomAlert(msg) {
-    setAlertMsg(msg);
     setShowAlert(true);
   }
   function closeAlert() {
@@ -53,37 +52,37 @@ function App() {
 
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.searchBarWrapper}>
-          <form onSubmit={handleSearch} className={styles.searchForm}>
+      <div className="container">
+        <div className="searchBarWrapper">
+          <form onSubmit={handleSearch} className="searchForm">
             <input
               type="text"
               value={city}
               onChange={handleInputChange}
               placeholder="Enter city name"
-              className={styles.input}
+              className="input"
             />
-            <button type="submit" className={styles.button}>
+            <button type="submit" className="button">
               Search
             </button>
           </form>
-          {loading && <p className={styles.loadingMsg}>Loading data...</p>}
+          {loading && <p className="loadingMsg">Loading data...</p>}
         </div>
         {weather && (
-          <div className={styles['weather-cards']}>
-            <div className={styles["weather-card"]}>
+          <div className="weather-cards">
+            <div className="weather-card">
               <b>Temperature</b>
               <div>{weather.temp}°C</div>
             </div>
-            <div className={styles["weather-card"]}>
+            <div className="weather-card">
               <b>Humidity</b>
               <div>{weather.humidity}%</div>
             </div>
-            <div className={styles["weather-card"]}>
+            <div className="weather-card">
               <b>Condition</b>
               <div>{weather.condition}</div>
             </div>
-            <div className={styles["weather-card"]}>
+            <div className="weather-card">
               <b>Wind Speed</b>
               <div>{weather.wind} kph</div>
             </div>
